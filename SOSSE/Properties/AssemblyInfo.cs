fcd2b03fa9473634f96d31b5f5492ee2820a4c03
Alex193a <alessandro193a@libero.it>
2016-01-25 14:42:14 +0100

    Update AssemblyInfo

--- SOSSE/Properties/AssemblyInfo.cs
+++ SOSSE/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Story of Seasons Save Editor")]
-[assembly: AssemblyDescription("Story of Seasons Save Editor")]
+[assembly: AssemblyTitle("Story of Seasons Save Editor EUR")]
+[assembly: AssemblyDescription("Story of Seasons Save Editor EUR")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Story of Seasons Save Editor")]
+[assembly: AssemblyProduct("Story of Seasons Save Editor EUR")]
 [assembly: AssemblyCopyright("Copyright ©  2015 RainThunder")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.2")]
+[assembly: AssemblyVersion("1.7.4")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
